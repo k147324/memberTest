46e9e24092b6920d481b3baf7defc0ff5eddd307
k147324 <10156115@gm.scu.edu.tw>
2026-09-08 07:07:30 +0800

    registerPage

--- src/app/components/register/register.ts
+++ src/app/components/register/register.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
-import { Form, FormsModule, Validators } from '@angular/forms';
+import { Form, FormsModule, Validators, FormControl, FormGroup, FormBuilder } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
-import { FormControl, FormGroup, Validator, FormBuilder } from '@angular/forms';
+
 @Component({
   selector: 'app-register',
   imports: [FormsModule, ReactiveFormsModule],
@@ -9,19 +9,21 @@ import { FormControl, FormGroup, Validator, FormBuilder } from '@angular/forms';
   styleUrl: './register.css',
 })
 export class Register {
+  baseURL: string = 'https://localhost:7058';
   private fb = inject(FormBuilder);
   registerData = this.fb.group({
     username: ['', Validators.required],
     password: ['', Validators.required],
     email: ['', Validators.required],
-    phone: ['', Validators.required, Validators.pattern('^09[0-9]{8}$')],
-    address: this.fb.group({
-      city: [''],
-      region: [''],
-      street: [''],
-      detail: [''],
-    }),
-    idnum: ['', Validators.required, Validators.pattern('^[A-Z][0-9]{9}$')],
+    phone: ['', [Validators.required, Validators.pattern('^09[0-9]{8}$')]],
+    address: [''],
+    idnum: ['', [Validators.required, Validators.pattern('^[A-Z][0-9]{9}$')]],
   });
-  Register() {}
+  Register() {
+    if (this.registerData.invalid) {
+      this.registerData.markAllAsTouched();
+      return;
+    }
+    console.log(this.registerData.value);
+  }
 }

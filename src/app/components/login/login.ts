559fa76c3653020d95567671e2414a6e8a8c5cdf
k147324 <10156115@gm.scu.edu.tw>
2026-09-10 07:49:58 +0800

    login

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -2,6 +2,7 @@ import { Register } from './../register/register';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { log } from 'console';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-login',
   imports: [FormsModule],
@@ -9,9 +10,30 @@ import { log } from 'console';
   styleUrl: './login.css',
 })
 export class Login {
+  constructor(private http: HttpClient) {}
+  baseURL: string = 'https://localhost:7058/api';
   loginData = { username: '', password: '' };
   errorMessage = '';
   login() {
-    this.errorMessage = this.loginData.password + this.loginData.username;
+    const payload = {
+      userName: this.loginData.username,
+      email: null,
+      password: this.loginData.password,
+    };
+    this.http.post<any>(`${this.baseURL}/UserAPI/Login`, payload).subscribe({
+      next: (res) => {
+        console.log('登入成功', res);
+        console.log('Token:', res.token); // 先在 console 看 token 長什麼樣子
+
+        // 之後要記得存起來,這裡先簡單測試
+        //localStorage.setItem('token', res.token);
+
+        this.errorMessage = '';
+      },
+      error: (err) => {
+        console.error('登入失敗', err);
+        this.errorMessage = err.error?.message || '登入失敗,請稍後再試';
+      },
+    });
   }
 }
---
+++ src/app/interfaces/UserInfoDTO.ts
@@ -0,0 +1,5 @@
+export interface UserInfoDTO {
+  Email: string;
+  Username: string;
+  Password: String;
+}
